91339eed15d3d754a4e7999a0aff1da0d360c36d
AllysomFernando <allysom.renczenczen18@gmail.com>
2025-08-27 18:27:15 -0300

    feat: creating the TicketBO at entities

--- DumuziTickets/src/domain/Entities/Ticket.cs
+++ DumuziTickets/src/domain/Entities/Ticket.cs
@@ -1,6 +1,34 @@
 namespace DumuziTickets.domain.entities;
 
-public class Ticket
+public class TicketBO : AbstractEntityBO<int>
 {
+    private FuncionarioBO _funcionario;
+    private int _quantidade;
+    private Situacao _situacao;
 
+    public TicketBO(int id, int quantidade, FuncionarioBO funcionario, Situacao situacao, DateTime createdAt,
+        DateTime updatedAt) : base(id, createdAt, updatedAt)
+    {
+        Funcionario = funcionario;
+        Quantidade = quantidade;
+        Situacao = situacao;
+    }
+
+    public FuncionarioBO Funcionario
+    {
+        get => _funcionario;
+        private set => _funcionario = value;
+    }
+
+    public int Quantidade
+    {
+        get => _quantidade;
+        private set => _quantidade = value;
+    }
+
+    public Situacao Situacao
+    {
+        get => _situacao;
+        private set => _situacao = value;
+    }
 }
