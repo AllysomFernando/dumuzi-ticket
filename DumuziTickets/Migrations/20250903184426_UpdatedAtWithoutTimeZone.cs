0e3ab1d6041f4cdcab5659e4000db8523faee0f4
AllysoRenczenczen <allysom.renczenczen18@gmail.com>
2025-09-03 16:29:48 -0300

    git init

---
+++ DumuziTickets/Migrations/20250903184426_UpdatedAtWithoutTimeZone.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DumuziTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedAtWithoutTimeZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "funcionario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Cpf = table.Column<string>(type: "text", nullable: false),
+                    Situacao = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_funcionario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticket",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FuncionarioId = table.Column<int>(type: "integer", nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    Situacao = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticket", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ticket_funcionario_FuncionarioId",
+                        column: x => x.FuncionarioId,
+                        principalTable: "funcionario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_funcionario_Cpf",
+                table: "funcionario",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticket_FuncionarioId",
+                table: "ticket",
+                column: "FuncionarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ticket");
+
+            migrationBuilder.DropTable(
+                name: "funcionario");
+        }
+    }
+}
